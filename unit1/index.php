30d2a3620bbaf6a2c837e5e110fa5aefcf4ab1d5
IhorMatsokha <59279790+IhorMatsokha@users.noreply.github.com>
2023-03-10 09:00:39 +0200

    Create index.php

---
+++ unit1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Unit_1</title>
+    <meta charset='utf-8'>
+</head>
+<body>
+<!--
+Завдання:
+1) Встановити програму для локального сервера WampServer (https://jak.koshachek.com/articles/wampserver-jak-vstanoviti-ta-nalashtuvati.html) або інший
+2) Пройтись по навченому матеріалові і спробувати їх на практиці
+-->
+<?php
+echo "Unit_1";
+echo "<br>";
+?>
+<?php
+echo 'Основний скрипт';
+include 'included.php';
+?>
+
+
+</body>
+</html>
