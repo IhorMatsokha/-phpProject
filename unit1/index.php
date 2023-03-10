e1ed8459419e8e2304c5f0398946003f0350ef9e
IhorMatsokha <59279790+IhorMatsokha@users.noreply.github.com>
2023-03-10 09:09:50 +0200

    Update index.php

--- unit1/index.php
+++ unit1/index.php
@@ -6,10 +6,7 @@
 </head>
 <body>
 <!--
-Завдання:
-1) Встановити програму для локального сервера WampServer (https://jak.koshachek.com/articles/wampserver-jak-vstanoviti-ta-nalashtuvati.html) або інший
-2) Пройтись по навченому матеріалові і спробувати їх на практиці
--->
+
 <?php
 echo "Unit_1";
 echo "<br>";
